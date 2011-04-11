cb00836a9e1491cb54cbd6b7e7b022c3ee46352b
Jon Kern <jonkern@comcast.net>
2011-04-10 22:55:15 -0400

    refining associations

--- user_event/app/model/user.rb
+++ user_event/app/model/user.rb
@@ -5,6 +5,23 @@ class User
   
   many :events
   
+  def likes
+    Event.where(:like_ids => id).all
+  end
+  
+  def attending
+    Event.where(:attendees => id).all
+  end
+
+  def interested_in
+    Event.where(:interested => id).all
+  end  
+  
+  def likes_event(event)
+    event.likes << self
+    event.save!
+  end
+  
   def to_s
     text = "\n" + "- "*25
     text += "\n#{name.upcase}"
@@ -14,6 +31,9 @@ class User
     else
       text += "\n  You have no events. Click *here* to get started!"
     end
+    text += "\n  You like #{likes.count} events" if likes.count > 0
+    text += "\n  You are attending #{attending.count} events" if attending.count > 0
+    text += "\n  You are interested in #{interested_in.count} events" if interested_in.count > 0
     text
   end
 end
--- user_event/spec/event_spec.rb
+++ user_event/spec/event_spec.rb
@@ -73,11 +73,8 @@ describe Event do
     }.to change {@event_2.likes.size}.by(1)
   end
   
-  # it "should provide like count for a user" do
-    # Event.like_count(@)
-  # end
-  
   after :all do
+    puts "EVENTS"
     events = Event.all
     events.each {|e| puts e}
     puts "USERS"
