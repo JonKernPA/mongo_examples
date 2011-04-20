91683755825516d00b0a47978b6b4bcbc1efacc6
Jon Kern <jonkern@comcast.net>
2011-04-19 23:02:34 -0400

    added date sort for event list_all

--- user_event/features/step_definitions/event_steps.rb
+++ user_event/features/step_definitions/event_steps.rb
@@ -1,3 +1,7 @@
+Given /^a clean database$/ do
+  User.destroy_all
+  Event.destroy_all
+end
 Given /^A User "([^"]*)"$/ do |user_name|
   user = User.create(:name => user_name)
 end
@@ -21,3 +25,30 @@ Then /^it should be on "([^"]*)"$/ do |date|
   @event.date.strftime("%m/%d/%Y").should == date
 end
 
+def dummy_word(len=6)
+  ('a'..'z').to_a.shuffle[0..len].join.capitalize
+end
+
+Given /^A set of events$/ do
+  secs_in_day = 24*60*60
+  fred = User.find_or_create_by_name("fred")
+  (1..10).each do 
+    Event.create(:title=>"#{dummy_word} #{dummy_word 3} #{dummy_word 10}", :date => Time.now + rand(60)*secs_in_day, :user => fred)
+  end
+  harry = User.find_or_create_by_name("harry")
+  (1..10).each do 
+    Event.create(:title=>"#{dummy_word} #{dummy_word 3} #{dummy_word 10}", :date => Time.now + rand(60)*secs_in_day, :user => harry)
+  end
+  Event.count.should == 20
+end
+
+When /^I display the events$/ do
+  @response = Event.list_all
+  puts @response
+end
+
+Then /^I should see them sorted by latest date first$/ do
+  first = Date.parse(@response.third[0..10])
+  last = Date.parse(@response.last[0..10])
+  first.should > last  
+end
